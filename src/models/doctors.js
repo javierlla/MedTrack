47855cb8a07c61041d87b88bc5cdd5a627ad41a4
Asier <acasiergonzalez@gmail.com>
2025-04-09 16:08:10 +0200

    Modelo Medicos creado

---
+++ src/models/doctors.js
@@ -0,0 +1,46 @@
+import { DataTypes } from "sequelize";
+import connection from "../config/db.js";
+
+const Doctor = connection.define("doctors",{
+    user_id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+        primaryKey: true
+    },
+
+    room_id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false
+    },
+
+    name: {
+        type: DataTypes.STRING(45),
+        allowNull: false
+    },
+
+    surname: {
+        type: DataTypes.STRING(45),
+        allowNull: false
+    },
+
+    speciality: {
+        type: DataTypes.STRING(45),
+        allowNull: false
+    },
+
+    telephone: {
+        type: DataTypes.STRING(45),
+        allowNull: false
+    }
+})
+
+Doctor.belongsTo(User,{foreignKey:"user_id"});
+User.hasOne(Doctor,{foreignKey:"user_id"});
+
+Appointment.belongsTo(Doctor,{foreignKey:"doctor_id"});
+Doctor.hasMany(Appointment,{foreignKey:"doctor_id"});
+
+Doctor.hasOne(Room,{foreignKey:"room_id"});
+Room.belongsTo(Doctor,{foreignKey:"room_id"});
+
+export default Doctor;
