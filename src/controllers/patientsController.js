4f1e5f5d72334124ed1eb7c0715273794d9695ff
ibonbautista <ibonbautista@gmail.com>
2025-04-09 17:16:02 +0200

    PatientsController creation

---
+++ src/controllers/patientsController.js
@@ -0,0 +1,18 @@
+import Patient from "../models/patients.js";
+
+function getAllPatients (){
+
+}
+
+function getPatientsByDoctor (doctor_id){
+
+}
+
+function getPatientsByUserId (id){
+
+}
+
+
+export default{
+    
+}
