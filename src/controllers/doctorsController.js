bfcbc009d9fdc7e2f3abf97a6780248ea1e1e165
Asier <acasiergonzalez@gmail.com>
2025-04-10 09:28:57 +0200

    Doctor controller actualizado

--- src/controllers/doctorsController.js
+++ src/controllers/doctorsController.js
@@ -1,25 +1,23 @@
 import Doctor from '../../models/doctors.js';
-import bcrypt from 'bcrypt';
 
 
 async function getAll(req,res){
     //res.send("Conseguir todos los stands");
     const doctors = await Doctor.findAll();
-    //console.log(stands);
-    //res.json(stands)
-    const role = req.session.user?.role;
-    res.render("doctor/list",{doctors});
+    //console.log(doctors);
+    res.json(doctors)
+    //res.render("doctor/list",{doctors});
 }
   
- async function getByID(req,res){
+async function getByID(req,res){
     const id = req.params.id;
-    //const {id} = req.params;
     //res.send("Conseguir el stand "+id);
-    const stand = await standModel.findByPk(id,{
-        include: [Product,StandCategory,Seller]
-    });
-    //res.json(stand);
-    res.render("stand/show",{stand}); // la ruta de render es a partir de la carpeta views, no la del router
+    const doctor = await Doctor.findByPk(id);
+    res.json(doctor);
+    //res.render("stand/show",{stand}); // la ruta de render es a partir de la carpeta views, no la del router
 }
-  
-  export default usuarioController;
+
+export default{
+    getAll,
+    getByID
+}
